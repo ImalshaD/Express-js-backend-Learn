bc2013191e3d7b9fb14f3a9b676d79c229e26694
ImalshaD <plid475@gmail.com>
2024-07-24 22:48:24 +0530

    first commit

---
+++ src/index.mjs
@@ -0,0 +1,29 @@
+import express from "express";
+import {query} from "express-validator";
+
+const app = express();
+app.use(express.json())
+
+const PORT = process.env.PORT || 3001;
+
+app.get("/", (request, response)=>{
+    response.status(405).send({"msg" : "Hello"})
+})
+
+app.get("/api/users", (request, response)=>{
+    const { query: {name},} = request;
+    console.log(name)
+    response.status(200).send({
+        "msg" : "Hello",
+        "name" : name
+    })
+})
+
+app.post("/api/users", (request, response)=>{
+    const person = request.body;
+    console.log(person);
+    response.status(200).send(person)
+})
+app.listen(PORT, ()=>{
+    console.log(`Running on Port ${PORT}.`)
+})
